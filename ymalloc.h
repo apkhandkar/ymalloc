5e723685a8ea9a8e618362689f90fcff11a9c1a4
Arjun Khandkar <khandkararjun@gmail.com>
2019-02-15 01:12:41 +0530

    removed a declaration for an internal function

--- ymalloc.h
+++ ymalloc.h
@@ -1,9 +1,11 @@
 #ifndef YMALLOC_H
 #define YMALLOC_H
 
-void * yalloc(ssize_t size);
+#include <stdlib.h>
+
 void * ymalloc(ssize_t size);
 void yfree(void * addr);
+void yfree_safe(void * addr);
 
 void mem_map(void);
 void ymalloc_summary(void);
